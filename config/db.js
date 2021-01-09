eccac89c4b097131966764811711937f4455b8b0
Pratik Shinde <pratikshinde1497@gmail.com>
2021-01-09 15:04:12 +0530

    add secure db data

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
+require("dotenv").config({path: "./config/config.env"})
+
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "postgres",
-    DB: "next_node_postgre",
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB,
+  dialect: process.env.DB_DIALECT,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
