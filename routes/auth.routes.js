8dc22ff2082fe5bb247b9267b5eaf9e61eb848f3
Pratik Shinde <pratikshinde1497@gmail.com>
2021-01-09 14:48:30 +0530

    add routes: auth, tutorial

---
+++ routes/auth.routes.js
@@ -0,0 +1,47 @@
+
+const users = require("../controllers/user.controller");
+const  passport = require('passport');
+var router = require("express").Router();
+
+
+//  @desc   Demo path for success on authenticate with google
+//  @route  GET   /api/v1/auth
+router.get('/profile', (req, res)=> { res.json({ success: true, data: req.user })})
+
+//  @desc   Authenticate with google
+//  @route  GET   /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile']} ))
+
+//  @desc   Google auth callback decide what  to do on success or failure
+//  @route  GET   /api/v1/auth/google/callback
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}), 
+(req, res) => {
+  req.user = res
+  res.redirect('/api/v1/auth/profile')
+})
+
+
+
+// Create a new Tutorial
+// router.post("/", users.create);
+
+// // Retrieve all Tutorials
+// router.get("/", users.findAll);
+
+
+// Retrieve all published Tutorials
+// router.get("/published", users.findAllPublished);
+
+// login user
+router.post("/login", users.findOne);
+
+// // Update a Tutorial with id
+// router.put("/:id", users.update);
+
+// // Delete a Tutorial with id
+// router.delete("/:id", users.delete);
+
+// // Create a new Tutorial
+// router.delete("/", users.deleteAll);
+
+module.exports = router
