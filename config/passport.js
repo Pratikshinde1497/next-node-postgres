443f4aefd4b8dac2d8270e1dcdbaac40510c2d5b
Pratik Shinde <pratikshinde1497@gmail.com>
2021-01-09 14:40:54 +0530

    add configs: database, passport, service urls

---
+++ config/passport.js
@@ -0,0 +1,49 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+require('dotenv').config({ path: "./config/config.env"})
+const { authenticateGoogleUser } = require("../middlewares/services/authentication_client")
+
+module.exports = function(passport) {
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/api/v1/auth/google/callback'
+  }, async (accessToken, refreshToken, profile, done)=> {
+    console.log(`
+    accessToken:  ${accessToken},
+    refreshToken: ${refreshToken}
+    `);
+    const newUser = {
+      googleId: profile.id,
+      displayName: profile.displayName,
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      image: profile.photos[0].value,
+      provider: profile.provider
+    }
+
+    //  user authentication service
+    try {
+      const {err, res} = authenticateGoogleUser(newUser);
+      if (res) {
+        done(null, res);
+      }
+      else throw err;
+      // authenticate_client.authenticateUserWithGoogle(newUser, (err, res) => {
+      //   if (!err) {
+      //     // console.log("from service: \n", res, "\n response done");
+      //     done(null, res);
+      //   }
+      // })
+    } catch (err) {
+      console.log(err);
+    }
+  }))
+
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+      done(null, user);
+  });
+}
