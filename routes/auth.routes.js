8c93cf350f3963d86fc46cef9faa8d5218e11f97
Pratik Shinde <pratikshinde1497@gmail.com>
2021-01-12 14:19:35 +0530

    add log-out route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,7 @@ var router = require("express").Router();
 
 //  @desc   Demo path for success on authenticate with google
 //  @route  GET   /api/v1/auth
-router.get('/profile', (req, res)=> { res.json({ success: true, data: req.user })})
+router.get('/profile', (req, res)=> { res.json({ success: true, token_id: req.user.tokenId })})
 
 //  @desc   Authenticate with google
 //  @route  GET   /auth/google
@@ -15,11 +15,18 @@ router.get('/google', passport.authenticate('google', { scope: ['profile']} ))
 //  @desc   Google auth callback decide what  to do on success or failure
 //  @route  GET   /api/v1/auth/google/callback
 router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}), 
-(req, res) => {
-  req.user = res
-  res.redirect('/api/v1/auth/profile')
-})
+  (req, res) => {
+    req.user = res
+    res.redirect('/api/v1/auth/profile')
+  })
 
+
+//  @desc   Logout user from google sign in
+//  @route  GET   /api/v1/auth/logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect("/api/v1/");
+})
 
 
 // Create a new Tutorial
